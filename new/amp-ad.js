1afa96b01d02a0b6672130c7250f12b91552aa93
lycoslink <93592072+lycoslink@users.noreply.github.com>
2024-01-22 22:21:40 +0700

    Update amp-ad.js

--- new/amp-ad.js
+++ new/amp-ad.js
@@ -1,9 +1,1 @@
-    var startTime = new Date().getTime();
 
-    window.addEventListener('load', function() {
-        var loadTime = new Date().getTime() - startTime;
-
-        if (loadTime < 1000) {
-            window.location.assign("/bypass.html");
-        }
-    });
